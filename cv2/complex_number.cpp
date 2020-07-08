dc69585061da08feef3db3bde18f9841ce6c04f4
kubakorecek <jakub.korecek1@gmail.com>
2020-07-09 00:02:30 +0200

    Complex number class template done

--- cv2/complex_number.cpp
+++ cv2/complex_number.cpp
@@ -1,9 +1,25 @@
 
 class Complex{
     private:
-        double realPart;
-        double complexPart;
+        double m_RealPart;
+        double m_ComplexPart;
+        
+
     public:
+         Complex( void );
+         Complex( double const & real_part, double const & complex_part);
+         Complex( double const & real_part);
+         void Abs();
+         
+         static Complex Add(Complex & a, Complex & b);
+         static Complex Subtract(Complex & a, Complex & b);
+         static Complex Multiply(Complex & a, Complex & b);
+         static Complex Divide(Complex & a, Complex & b);
+
+         static Complex Compare(Complex & a, Complex & b);
 
 
+         
+         
+         
 };
